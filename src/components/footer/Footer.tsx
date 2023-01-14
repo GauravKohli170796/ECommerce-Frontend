262b6a07f6d479f9e4d8f1681bbad8360eb0b7d1
gaurav.kohli.1796 <gaurav.kohli@measured.com>
2023-01-14 19:33:01 +0530

    added my image in footer.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,11 @@
-import { Typography } from '@mui/material';
+import { Avatar, Typography } from '@mui/material';
 import { Box, Stack } from '@mui/system';
 import React from 'react';
 
 function Footer() {
     return (
-        <Box className="fCenter fCol mixBackground p-2 my-2" sx={{color:"white"}}>
-                {/* <div className="grid-container">
+        <Box className="fCenter fCol mixBackground p-2 my-2" sx={{ color: "white" }}>
+            {/* <div className="grid-container">
                     <div className="grid-ele" style={{ gridRow: "1/2" }}>DEVELOPED BY,</div>
                     <div className="grid-ele" style={{ gridRow: "2/3" }}>Gaurav Kohli (NIT Kurukshetra | Software Developer at Pine Labs)
                     </div>
@@ -14,13 +14,18 @@ function Footer() {
 
                 </div> */}
 
-                <Stack direction="column" spacing={3}>
-                    <Typography sx={{textAlign:"center"}}>DEVELOPED BY,</Typography>
-                    <Typography sx={{textAlign:"center"}}>Gaurav Kohli (NIT Kurukshetra | Software Developer at Pine Labs)</Typography>
-                    <Typography sx={{textAlign:"center"}}>© COPYRIGHT</Typography>
-                    <Typography sx={{textAlign:"center"}}>All Rights Reserved</Typography>
+                <Avatar
+                    alt="Gaurav Kohli"
+                    src="https://res.cloudinary.com/dnqwvvtqf/image/upload/v1673704357/ProductTest/pf51bw72wjas9nfh46vx.webp"
+                    sx={{ width: 100, height: 100}}
+                />
+            <Stack direction="column" spacing={3}>
+                <Typography sx={{ textAlign: "center" }}>DEVELOPED BY,</Typography>
+                <Typography sx={{ textAlign: "center" }}>Gaurav Kohli (NIT Kurukshetra | X Software Developer at Pine Labs | Senior Software Enginner at Tech Prescient)</Typography>
+                <Typography sx={{ textAlign: "center" }}>© COPYRIGHT</Typography>
+                <Typography sx={{ textAlign: "center" }}>All Rights Reserved</Typography>
 
-                </Stack>
+            </Stack>
         </Box>
     )
 }
